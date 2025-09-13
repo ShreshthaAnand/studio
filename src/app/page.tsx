d578acb4b9297efa9dcc7441c83860c78d053765
Shreshtha Anand <anandshreshtha8@gmail.com>
2025-09-13 07:02:44 +0000

    give me some more colums

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
              </CardHeader>
              <CardContent className="p-6">
               <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                  {Array.from({ length: 8 }).map((_, index) => (
+                  {Array.from({ length: 12 }).map((_, index) => (
                     <ImageUploader key={index} onUpload={handleSelectImage} onDeselect={handleDeselectImage} />
                   ))}
               </div>
